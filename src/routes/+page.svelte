1d73897a9ca55227c13cf0798bd88a63ff1b2a9c
Richard Andrew Long <idrew@Richards-Mac-mini.local>
2024-02-10 20:10:57 -0600

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Hero from "$lib/hero.svelte";
+
+
+
+</script>
+
+<Hero/>
+
+<svelte:head>
+  
+
+  <!-- Open Graph meta tags -->
+  <meta property="og:title" content="iD-Dev | Website Coming Soon..">
+  <meta property="og:description" content="Web Developer | Multi-Creative.">
+  <meta property="og:image" content="/IDLOGO.png">
+  <meta property="og:url" content="https://www.id-dev.com">
+  <meta property="og:type" content="website">
+
+</svelte:head>
